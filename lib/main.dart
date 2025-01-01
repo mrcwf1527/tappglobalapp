c0b8bb5a9e2cafb5c583a6f409427ab96cf118ae
Ryan Chan <ryan@finextapp.com>
2025-01-01 23:01:06 +0800

    refactor: move Firebase API keys to environment variables

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Add this import
 import 'config/theme.dart';
 import 'config/routes.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");  // Add this line
   try {
     await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
     debugPrint('Firebase initialized successfully');
